718e5e3aaabdf275d19f1f59e66e39dee610877a
Vivek <vr7064300@gmail.com>
2024-05-17 23:50:54 +0530

    just random

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/home", (req, res) => {
+  res.send("<h1>Hello, how are you</h1>");
+});
+
+app.get("/linkdIn", (req, res) => {
+  res.json({ name: "vivek", profession: "student" });
+});
+
+app.get("/file", (req, res) => {
+  const filepath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filepath);
+  console.log(path);
+  console.log(filepath);
+  console.log(__dirname);
+  console.log(__filename);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
